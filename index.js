d5da4ece66b64d74b131e577b2ad493938abd2f2
teahuhu <34065651+teahuhu@users.noreply.github.com>
2017-12-29 17:00:07 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,34 @@
+
+const express = require('express');
+const app = express()
+const path = require('path');
+const bodyparser = require('body-parser');
+const mongo = require('./mongodb');
+
+const collectionName = 'gushi';
+
+app.use('/', express.static(path.join(__dirname, 'public')))
+app.use(bodyparser.json());
+
+app.get("/api/poems", (req, res) => {
+	mongo.find(collectionName)
+		.then((data) => res.json(data))
+		.catch((err) => res.json([]))
+})
+
+app.get("/api/poems/:id", (req, res) => {
+	console.log(req.params.id)
+	mongo.find(collectionName, {text: req.params.id})
+		.then((data) => {
+			res.json(data && data !== undefined && data.length > 0 ? 'Found' : 'Not Found')
+		})
+		.catch((err) => res.json('Not Found'))
+})
+
+app.post("/api/poems", (req, res) => {
+	mongo.create(collectionName, req.body)
+		.then((data) => res.json({ result: 'success' }))
+		.catch((err) => res.json({ result: 'fail' }))
+})
+
+app.listen(3000, () => { console.log('Server running..') })
