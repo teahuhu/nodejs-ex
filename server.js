5dbe9f48695d905a1283cae34625b3aa021c3973
teahuhu <34065651+teahuhu@users.noreply.github.com>
2018-01-02 11:30:50 -0500

    Use Local memory instead of MongoDB

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var _ = require('lodash');
 var mongodb = require("mongodb");
 var ObjectID = mongodb.ObjectID;
 
@@ -12,28 +13,13 @@ app.use(bodyParser.json());
 // Create link to html directory
 app.use('/', express.static(path.join(__dirname, 'public')))
 
-// Create a database variable outside of the database connection callback to reuse the connection pool in your app.
-var db;
+var poems = [];
 
-// Connect to the database before starting the application server.
-//mongodb.MongoClient.connect('mongo "mongodb://teahuhu-shard-00-00-pkqcy.mongodb.net:27017,teahuhu-shard-00-01-pkqcy.mongodb.net:27017,teahuhu-shard-00-02-pkqcy.mongodb.net:27017/test?replicaSet=teahuhu-shard-0" --ssl --authenticationDatabase admin --username teahuhu --password chengyu88' || 'mongodb://localhost:27017/messages', function (err, database) {
-//  if (err) {
-//    console.log(err);
-//    process.exit(1);
-//  }
-
-  // Save database object from the callback for reuse.
-//  db = database;
-//  console.log("Database connection ready");
-
-  // Initialize the app.
-  var server = app.listen(process.env.PORT || 8080, function () {
-    var port = server.address().port;
-    console.log("App now running on port", port);
-  });
-//});
-
-// CONTACTS API ROUTES BELOW
+// Initialize the app.
+var server = app.listen(process.env.PORT || 8080, function () {
+  var port = server.address().port;
+  console.log("App now running on port", port);
+});
 
 // Generic error handler used by all endpoints.
 function handleError(res, reason, message, code) {
@@ -41,41 +27,20 @@ function handleError(res, reason, message, code) {
   res.status(code || 500).json({"error": message});
 }
 
-/*  "/api/contacts"
- *    GET: finds all contacts
- *    POST: creates a new contact
- */
-
 app.get("/api/poems", function(req, res) {
-  db.collection(POEM_COLLECTION).find({}).toArray(function(err, docs) {
-    if (err) {
-      handleError(res, err.message, "Failed to get poems.");
-    } else {
-      res.status(200).json(docs);
-    }
-  });
+  res.status(200).json(poems);
 });
 
 app.post("/api/poems", function(req, res) {
   var newPoem = req.body;
-
-  db.collection(POEM_COLLECTION).insertOne(newPoem, function(err, doc) {
-    if (err) {
-      handleError(res, err.message, "Failed to create new poem.");
-    } else {
-      res.status(201).json(doc.ops[0]);
-    }
-  });
+  poems = _.concat(poems, newPoem);
+  res.status(201).json(newPoem);
 });
 
 app.get("/api/poems/:id", function(req, res) {
+  var poemIndex = _.findIndex(poems, function(p) {
+    return p.text === req.params.id;
+  })
+  res.status(200).json((poemIndex > -1  ? 'Found' : 'Not Found'));
   console.log(req.params.id)
-  db.collection(POEM_COLLECTION).findOne({text: req.params.id}, function(err, doc) {
-    console.log(doc)
-    if (err) {
-      handleError(res, err.message, "Failed to get poem");
-    } else {
-      res.status(200).json((doc && doc !== undefined ? 'Found' : 'Not Found'));
-    }
-  });
 });
