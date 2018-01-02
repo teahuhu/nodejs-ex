8cb3f31c8cdc5b1821267ef5c0460489f09e366e
teahuhu <34065651+teahuhu@users.noreply.github.com>
2018-01-02 13:48:50 -0500

    Use json file to store data

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var _ = require('lodash');
+var fs = require('fs');
+
 var mongodb = require("mongodb");
 var ObjectID = mongodb.ObjectID;
 
 var POEM_COLLECTION = "gushi";
+var POEM_FILE = "./poems.json";
 
 var app = express();
 var path = require('path');
@@ -28,19 +31,54 @@ function handleError(res, reason, message, code) {
 }
 
 app.get("/api/poems", function(req, res) {
-  res.status(200).json(poems);
+  //res.status(200).json(poems);
+  fs.readFile(POEM_FILE, 'utf8', function(err, contents) {
+    if (err) {
+      console.log('CODE: ' + err.code);
+      console.log('ERROR: ' + err);
+      res.status(500).json('Error: ' + err);
+      return;
+    }       
+    var allPoems = JSON.parse(contents);
+    res.status(200).json(allPoems);
+  });
 });
 
 app.post("/api/poems", function(req, res) {
   var newPoem = req.body;
-  poems = _.concat(poems, newPoem);
-  res.status(201).json(newPoem);
+  //poems = _.concat(poems, newPoem);
+  //res.status(201).json(newPoem);
+  fs.readFile(POEM_FILE, 'utf8', function(err, contents) {
+    if (err && err.code !== 'ENOENT') {
+      console.log('ERROR: ' + err);
+      res.status(500).json('Error: ' + err);
+      return;
+    }    
+    var allPoems = contents && contents !== undefined ? JSON.parse(contents) : [];
+    allPoems = _.concat(allPoems, newPoem);
+    fs.writeFile(POEM_FILE, JSON.stringify(allPoems), 'utf8', function(err) {
+      if (err) { 
+        console.log('ERROR: ' + err);
+        res.status(500).json('Error: ' + err);
+        return;
+      }
+      res.status(201).json(newPoem);
+    });
+  });
 });
 
 app.get("/api/poems/:id", function(req, res) {
-  var poemIndex = _.findIndex(poems, function(p) {
-    return p.text === req.params.id;
-  })
-  res.status(200).json((poemIndex > -1  ? 'Found' : 'Not Found'));
-  console.log(req.params.id)
+  fs.readFile(POEM_FILE, 'utf8', function(err, contents) {
+    if (err) {
+      console.log('ERROR: ' + err);
+      res.status(200).json('Not Found-Error: ' + err);
+      return;
+    }
+    var allPoems = JSON.parse(contents);
+    var poemIndex = _.findIndex(allPoems, function(p) {
+      return p.text === req.params.id;
+    })
+    res.status(200).json((poemIndex > -1  ? 'Found' : 'Not Found'));
+  });  
+
 });
